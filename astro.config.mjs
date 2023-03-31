66840f7e9de9175948a247ed7f7f22ca31587dac
Compey <hi@devcomp.xyz>
2023-03-31 13:51:21 +0530

    remove unused dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-
-import mdx from "@astrojs/mdx"; // https://astro.build/config
-
-import react from "@astrojs/react";
+import mdx from "@astrojs/mdx"; 
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), mdx(), react()]
+  integrations: [tailwind(), svelte(), mdx()]
 });
