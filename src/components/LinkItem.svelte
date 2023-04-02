c1e8a6670fcfd740edba3eb39bc123754cc4c6fb
Compey <hi@devcomp.xyz>
2023-04-02 20:43:09 +0530

    add `rel=me` for mastodon verification

--- src/components/LinkItem.svelte
+++ src/components/LinkItem.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <li>
-	<a {href} target="_blank" rel="noreferrer noopener" class="dark:text-ocean-300 link underline"
+	<a {href} target="_blank" rel="noreferrer noopener me" class="dark:text-ocean-300 link underline"
 		>{name}</a
 	>
 	{#if description}
