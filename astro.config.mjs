fafea9179e07e96e7f77bc750dcb34551417ba42
Erica Marigold <hi@devcomp.xyz>
2023-06-10 11:25:33 +0530

    updated config: SSR & no mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-import mdx from "@astrojs/mdx"; 
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), mdx()]
+  integrations: [tailwind(), svelte()],
+  output: 'server'
 });
