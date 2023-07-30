4a3b478ca0cfbc59f20ef929b3895f5ff946b230
Compey <hi@devcomp.xyz>
2023-07-30 23:06:28 +0530

    chore: remove generated vercel build

--- .vercel/output/functions/render.func/chunks/_slug_@_@astro.76549074.mjs
+++
@@ -1,7 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-import './astro.4ba9c02c.mjs';
-
-const page = () => import('./pages/_slug_.astro.0a7847ff.mjs');
-
-export { page };
--- .vercel/output/functions/render.func/chunks/index@_@astro.9cf63396.mjs
+++
@@ -1,7 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-import './astro.4ba9c02c.mjs';
-
-const page = () => import('./pages/index.astro.0af53dbe.mjs');
-
-export { page };
